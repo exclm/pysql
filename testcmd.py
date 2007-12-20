58a4e0031d8ff0bd0699e931a4c1d381ecf2aa75
devlinjs <devlinjs@FA7CZA6N1254998.wrightpatterson.afmc.ds.af.mil>
2007-12-20 13:48:56 -0500

    test files

---
+++ testcmd.py
@@ -0,0 +1,18 @@
+import cmd2
+
+class test(cmd2.Cmd):
+    binarypar = True
+    settable = ['prompt', 'binarypar']
+    def do_sing(self, arg):
+        '''Sings.  
+        
+        Hums if it has to fake it.'''
+        print arg or 'hmm hmm hmm'
+    def do_shout(self, arg):
+        print arg.upper()
+    def do_quit(self, arg):
+        return 1
+    multilineCommands = ['shout']
+
+t = test()
+t.cmdloop()
