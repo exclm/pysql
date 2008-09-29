a41d2aa3b10b8309b1cb5dfc397293ecef6eedf3
catherine <catherine@dellzilla>
2008-09-29 14:50:31 -0400

    adding bar charts

--- sqlpython/editplot.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python
-import plothandler
-plothandler.Plot().unshelve()
--- sqlpython/plothandler.py
+++ sqlpython/plothandler.py
@@ -9,7 +9,8 @@ try:
             self.legends = []
             self.yserieslists = []
             self.xticks = []
-        def build(self, sqlSession):
+        def build(self, sqlSession, outformat):
+            self.outformat = outformat
             self.title = sqlSession.tblname
             self.xlabel = sqlSession.curs.description[0][0]
             self.datatypes = [d[1] for d in sqlSession.curs.description]
@@ -26,7 +27,7 @@ try:
                 self.xticks = [r[0] for r in sqlSession.rows]
         def shelve(self):
             s = shelve.open(shelvename,'c')
-            for k in ('xvalues xticks yserieslists title legends xlabel'.split()):
+            for k in ('xvalues xticks yserieslists title legends xlabel outformat'.split()):
                 s[k] = getattr(self, k)
             s.close()
             # reading pickles fails with EOF error, don't understand
@@ -34,19 +35,36 @@ try:
             s = shelve.open(shelvename)
             self.__dict__.update(s)
             s.close()
-            self.draw()            
-        def draw(self):
+            self.draw()
+        def bar(self):
+            barEdges = pylab.arange(len(self.xvalues))
+            width = 0.5
+            for yseries in self.yserieslists:
+                pylab.bar(barEdges, yseries, width=width)
+            if self.xticks:
+                pylab.xticks(barEdges + (0.5 * width), self.xticks)            
+        def line(self, markers):
+            for yseries in self.yserieslists:
+                pylab.plot(self.xvalues, yseries, markers)
+            if self.xticks:
+                pylab.xticks(self.xvalues, self.xticks)
+        def draw(self, chartGuts, markers=None):
             if not self.yserieslists:
                 print 'At least one quantitative column needed to plot.'
                 return None
-            for yseries in self.yserieslists:
-                pylab.plot(self.xvalues, yseries, '-o')
-            if self.xticks:
-                pylab.xticks(self.xvalues, self.xticks)
+            if self.outformat == '\\p':
+                self.line('-o')
+            elif self.outformat == '\\P':
+                self.line('-')
+            else:
+                self.bar()
             pylab.xlabel(self.xlabel)
             pylab.title(self.title)
             pylab.legend(self.legends)
             pylab.show()
+            print 'You can edit this plot from the command prompt (outside sqlpython) by running'
+            print "ipython -pylab -c 'import sqlpython.plothandler; sqlpython.plothandler.Plot().unshelve()'"
+            # there's got to be a way to install a shell script like that through setuptools... but how?
             
 except ImportError:
     class Plot(object):
