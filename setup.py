ce618c2b90b6d685602096dd2ee40361f25e40be
catherine <catherine@dellzilla>
2008-11-17 13:14:29 -0500

    sending changes to dellzilla

--- setup.py
+++ setup.py
@@ -9,21 +9,21 @@ Topic :: Database :: Front-Ends
 Operating System :: OS Independent""".splitlines()
 
 setup(name="sqlpython",
-      version="1.4.8",
+      version="1.5.0",
       description="Command-line interface to Oracle",
       long_description="Customizable alternative to Oracle's SQL*PLUS command-line interface",
       author="Luca Canali",
       author_email="luca.canali@cern.ch",
       url="https://twiki.cern.ch/twiki/bin/view/PSSGroup/SqlPython",
       packages=find_packages(),
-      py_modules = ['mysqlpy','completion','sqlpyPlus','sqlpython','pexpecter'],    
       include_package_data=True,    
-      install_requires=['pyparsing','cmd2>=0.3.5','cx_Oracle'],
+      install_requires=['pyparsing','cmd2>=0.4.3','cx_Oracle','genshi>=0.5'],
       keywords = 'client oracle database',
       license = 'MIT',
       platforms = ['any'],
       entry_points = """
                    [console_scripts]
-                   sqlpython = mysqlpy:run"""      
+                   sqlpython = sqlpython.mysqlpy:run
+                   editplot_sqlpython = sqlpython.editplot.bash"""      
      )
 
