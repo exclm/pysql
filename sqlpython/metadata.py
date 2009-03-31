89367593c9f16ce64455048d16148debae819e11
Catherine Devlin <catherine.devlin@gmail.com>
2009-03-31 13:02:34 -0400

    finished replacing descQueries with metaqueries

--- sqlpython/metadata.py
+++ sqlpython/metadata.py
@@ -3,8 +3,8 @@ from collections import defaultdict
 metaqueries = defaultdict(defaultdict)
 
 metaqueries['desc']['oracle'] = defaultdict(defaultdict)
-metaqueries['desc']['oracle']['TABLE']['long'] = """
-SELECT atc.column_id "#",
+metaqueries['desc']['oracle']['TABLE']['long'] = (
+"""SELECT atc.column_id "#",
 atc.column_name,
 CASE atc.nullable WHEN 'Y' THEN 'NULL' ELSE 'NOT NULL' END "Null?",
 atc.data_type ||
@@ -22,10 +22,10 @@ FROM all_tab_columns atc
 JOIN all_col_comments acc ON (acc.owner = atc.owner AND acc.table_name = atc.table_name AND acc.column_name = atc.column_name)
 WHERE atc.table_name = :object_name
 AND      atc.owner = :owner
-ORDER BY atc.column_id;"""
+ORDER BY atc.column_id;""",)
 
-metaqueries['desc']['oracle']['TABLE']['short'] = """
-SELECT atc.column_name,
+metaqueries['desc']['oracle']['TABLE']['short'] = (
+"""SELECT atc.column_name,
 CASE atc.nullable WHEN 'Y' THEN 'NULL' ELSE 'NOT NULL' END "Null?",
 atc.data_type ||
 CASE atc.data_type WHEN 'DATE' THEN ''
@@ -40,10 +40,10 @@ data_type
 FROM all_tab_columns atc
 WHERE atc.table_name = :object_name
 AND      atc.owner = :owner
-ORDER BY atc.column_id;"""
+ORDER BY atc.column_id;""",)
 
-metaqueries['desc']['oracle']['PROCEDURE'] = """
-SELECT NVL(argument_name, 'Return Value') argument_name,             
+metaqueries['desc']['oracle']['PROCEDURE'] = (
+"""SELECT NVL(argument_name, 'Return Value') argument_name,             
 data_type,
 in_out,
 default_value
@@ -51,16 +51,16 @@ FROM all_arguments
 WHERE object_name = :object_name
 AND      owner = :owner
 AND      package_name IS NULL
-ORDER BY sequence;"""
+ORDER BY sequence;""",)
 
-metaqueries['desc']['oracle']['PackageObjects'] = """
-SELECT DISTINCT object_name
+metaqueries['desc']['oracle']['PackageObjects'] = (
+"""SELECT DISTINCT object_name
 FROM all_arguments
 WHERE package_name = :package_name
-AND      owner = :owner"""
+AND      owner = :owner""",)
 
-metaqueries['desc']['oracle']['PackageObjArgs'] = """
-SELECT object_name,
+metaqueries['desc']['oracle']['PackageObjArgs'] = (
+"""SELECT object_name,
 argument_name,             
 data_type,
 in_out,
@@ -70,4 +70,44 @@ WHERE package_name = :package_name
 AND      object_name = :object_name
 AND      owner = :owner
 AND      argument_name IS NOT NULL
-ORDER BY sequence;"""
+ORDER BY sequence;""",)
+
+metaqueries['desc']['oracle']['TRIGGER'] = (
+"""SELECT description
+FROM   all_triggers
+WHERE  owner = :owner
+AND    trigger_name = :object_name;
+""",
+"""SELECT table_owner,
+base_object_type,
+table_name,
+column_name,
+when_clause,
+status,
+action_type,
+crossedition
+FROM   all_triggers
+WHERE  owner = :owner
+AND    trigger_name = :object_name
+\\t""",)
+
+
+metaqueries['desc']['oracle']['INDEX'] = (
+"""SELECT index_type,
+table_owner,
+table_name,
+table_type,
+uniqueness,
+compression,
+partitioned,
+temporary,
+generated,
+secondary,
+dropped,
+visibility
+FROM   all_indexes
+WHERE  owner = :owner
+AND    index_name = :object_name\\t""",)
+
+metaqueries['desc']['oracle']['VIEW'] = metaqueries['desc']['oracle']['TABLE']['short']
+metaqueries['desc']['oracle']['FUNCTION'] = metaqueries['desc']['oracle']['PROCEDURE']
