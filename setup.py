9b8f77678d87452f61de191e6de60c96271d6885
catherine <catherine@cordelia>
2009-04-29 08:36:09 -0400

    making the nogerald branch

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ Topic :: Database :: Front-Ends
 Operating System :: OS Independent""".splitlines()
 
 setup(name="sqlpython",
-      version="1.6.4",
+      version="1.6.5",
       description="Command-line interface to Oracle",
       long_description="Customizable alternative to Oracle's SQL*PLUS command-line interface",
       author="Luca Canali",
@@ -17,7 +17,7 @@ setup(name="sqlpython",
       url="http://packages.python.org/sqlpython",
       packages=find_packages(),
       include_package_data=True,    
-      install_requires=['pyparsing','cmd2>=0.5.2','cx_Oracle','genshi>=0.5','sqlalchemy','gerald'],
+      install_requires=['pyparsing','cmd2>=0.5.2','cx_Oracle','genshi>=0.5','sqlalchemy'],
       keywords = 'client oracle database',
       license = 'MIT',
       platforms = ['any'],
