251a4f5b238e250440f0ce27bb7ef6e857bad038
catherine <catherine@cordelia>
2009-09-02 09:44:08 -0400

    v1.6.8

--- sqlpython/sqlpython.py
+++ sqlpython/sqlpython.py
@@ -1,7 +1,7 @@
 #
-# SqlPython V1.6.7
+# SqlPython V1.6.8
 # Author: Luca.Canali@cern.ch, Apr 2006
-# Rev 7-Jul-09
+# Rev 2-Sep-09
 
 # A python module to reproduce Oracle's command line 'sqlplus-like' within python
 # Intended to allow easy customizations and extentions 
@@ -10,7 +10,7 @@
 
 import cmd2,getpass,binascii,cx_Oracle,re,os,platform
 import sqlalchemy, pyparsing
-__version__ = '1.6.7'    
+__version__ = '1.6.8'    
 
 class Parser(object):
     comment_def = "--" + ~ ('-' + pyparsing.CaselessKeyword('begin')) + pyparsing.ZeroOrMore(pyparsing.CharsNotIn("\n"))    
