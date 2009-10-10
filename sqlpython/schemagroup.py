81fa28859af7ea294e546519796d56f8a374788c
catherine <catherine@cordelia>
2009-10-09 23:08:50 -0400

    nice progress on tab completion

--- sqlpython/schemagroup.py
+++ sqlpython/schemagroup.py
@@ -1,8 +1,23 @@
-import gerald, re, datetime, threading, time
+import gerald, re, datetime, threading, time, operator
 
 def gerald_connection_string(sqlalchemy_connection_string):
     return sqlalchemy_connection_string.split('/?mode=')[0].replace('//','/')
-    
+
+def build_column_list(schema):
+    schema.column_names = [[c for c in o.columns] for o in schema.schema.values() 
+                           if hasattr(o, 'columns') 
+                           and hasattr(o.columns, 'keys')]
+    schema.column_names = reduce(operator.add, schema.column_names, [])
+    schema.table_names = []
+    schema.qual_table_names = []
+    for t in schema.schema.values():
+        if hasattr(t, 'columns') and isinstance(t.columns, dict):
+            schema.table_names.append(t.name)
+            schema.qual_table_names.append('%s.%s' % (schema.name, t.name))
+            for c in t.columns:
+                schema.column_names.append('%s.%s' % (t.name, c))
+    schema.qual_column_names = ['%s.%s' % (schema.name, c) for c in schema.column_names]
+        
 class RefreshThread(threading.Thread):
     def __init__(self, schemagroup, owner):
         threading.Thread.__init__(self)
@@ -14,29 +29,33 @@ class RefreshThread(threading.Thread):
             self.schema = self.schemagroup.childtype(self.owner, self.schemagroup)
         else:
             self.schema.refreshed = self.schema.time()
-        self.schemagroup.schemas[self.owner] = self.schema
-            
+        self.schemagroup.schemas[self.owner] = self.schema                                                
+        build_column_list(self.schema)
+        
 class RefreshGroupThread(threading.Thread):
-    def __init__(self, schemas, minutes_between_refreshes):
+    def __init__(self, schemas):
         threading.Thread.__init__(self)
         self.parent = threading.current_thread()
         self.schemas = schemas
-        self.minutes_between_refreshes = minutes_between_refreshes
         self.daemon = True
     def run(self):
-        while self.parent.isAlive():
-            self.schemas.refresh()
-            time.sleep(60 * self.minutes_between_refreshes)
+        self.schemas.refresh()
+        s.column_names = reduce(operator.add, 
+                                [s.qual_column_names for s in self.schemas.values()],
+                                [])
+        s.table_names = reduce(operator.add, 
+                               [s.qual_table_names for s in self.schemas.values()],
+                               [])
         
 class SchemaDict(dict):
     schema_types = {'oracle': gerald.OracleSchema}
-    def __init__(self, dct, rdbms, user, connection, connection_string, minutes_between_refreshes):
+    def __init__(self, dct, rdbms, user, connection, connection_string):
         dict.__init__(self, dct)
         self.child_type = self.schema_types[rdbms]
         self.user = user
         self.connection = connection
         self.gerald_connection_string = gerald_connection_string(connection_string)
-        self.refresh_thread = RefreshGroupThread(self, minutes_between_refreshes)
+        self.refresh_thread = RefreshGroupThread(self)
         self.complete = 0
     def refresh_asynch(self):
         self.refresh_thread.start()
@@ -59,13 +78,16 @@ class SchemaDict(dict):
                 # what if a user's last object is deleted?
             if isinstance(self.complete, int):
                 self.complete += 1
+        self.column_names = [s.column_names for s in self.values()]
+        self.columns = reduce(operator.add, [s.column_names for s in self.values()])
         self.complete = 'all'
+        print 'metadata discovered'
     def refresh_one(self, owner, current_database_time=None):
         if not current_database_time:
             current_database_time = self.get_current_database_time()
         self[owner] = self.child_type(owner, self.gerald_connection_string)
         self[owner].refreshed = current_database_time        
-        
+        build_column_list(self[owner])
 
 class PlainObject(object):
     '''Simply a dumb container for attributes.'''
@@ -108,3 +130,14 @@ class MetaData(PlainObject):
     def descriptor(self, qualified=False):
         return '%s/%s' % (self.db_type, self.name(qualified))
         
+s = None
+
+if __name__ == '__main__':
+    connection_string = 'oracle://jrrt:password@orcl/?mode=0'
+    connection = None
+    dct = {}
+    user = 'jrrt'
+    rdbms = 'oracle'
+    s = SchemaDict(dct, rdbms, user, connection, connection_string, 100)
+    sch = s.child_type('jrrt',s.gerald_connection_string)
+    
